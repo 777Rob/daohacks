19a504665d1d4ddc20e14e63a8701d7449655310
Your Name <you@example.com>
2022-04-10 06:43:15 +0200

    next+ts

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+// TODO ADD GRAPH DISPLAY ALL USER NFTS AS BUTTONS
+
 export default function Home() {
   return (
     <div className={styles.container}>
